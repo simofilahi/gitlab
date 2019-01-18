fe1abd21b0462ba216ba1cf30db62a66c77569de
Mohamed Filahi <mfilahi@e1r11p1.1337.ma>
2019-01-18 16:58:55 +0100

    t

--- gitlab.rb
+++ gitlab.rb
@@ -367,7 +367,7 @@ external_url 'https://gitlab.example.com'
 # high_availability['mountpoint'] = ["/var/opt/gitlab/git-data", "/var/opt/gitlab/gitlab-rails/shared"]
 
 ### GitLab Shell settings for GitLab
-# gitlab_rails['gitlab_shell_ssh_port'] = 22
+gitlab_rails['gitlab_shell_ssh_port'] = 222
 # gitlab_rails['gitlab_shell_git_timeout'] = 800
 
 ### Extra customization
